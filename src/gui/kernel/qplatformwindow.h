cd8aa89cb2fd052ce6a3b8c2bb734979ded81978
Morten Johan Sørvig <morten.sorvig@digia.com>
2014-12-01 13:26:53 +0100

    Add QPlatformWindow::window* accessors.
    The platform plugins should use this to get geometry
    and sizes in device pixels. (The corresponding QWindow
    accessors return values in device-independent pixels).
    
    This should be easier to use from the platform plugin
    implementors point of view: there is no longer a need
    to pick and use one of the qHighDpi* functions. We
    can also hide implementation details like the "Constrained"
    functions and normalization.

--- src/gui/kernel/qplatformwindow.h
+++ src/gui/kernel/qplatformwindow.h
@@ -129,6 +129,13 @@ public:
     static QRect initialGeometry(const QWindow *w,
         const QRect &initialGeometry, int defaultWidth, int defaultHeight);
 
+    // Window property accessors. Platform plugins should use these
+    // instead of accessing QWindow directly.
+    QSize windowMinimumSize() const;
+    QSize windowMaximumSize() const;
+    QSize windowBaseSize() const;
+    QSize windowSizeIncrement() const;
+    QRect windowGeometry() const;
 protected:
     static QString formatWindowTitle(const QString &title, const QString &separator);
     QPlatformScreen *screenForGeometry(const QRect &newGeometry) const;
